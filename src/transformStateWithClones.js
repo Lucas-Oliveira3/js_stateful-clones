4fa21cf9cd9d3a6456a37458ffb9a47b81349e59
Mr-kzen <lukaszzzen@gmail.com>
2026-03-02 19:54:43 -0300

    Resolução do problema juntamente ao tutor!

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,51 +7,36 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
   const history = [];
-  // Começamos com um clone para garantir a imutabilidade do original
   let stateCopy = { ...state };
 
-  // assegurando actions
-  // eslint-disable-next-line no-param-reassign
-  actions = Array.isArray(actions) ? actions : [];
+  const safeActions = Array.isArray(actions) ? actions : [];
 
-  // laço que irá realizar uma varredura em todas as actions
-  for (const action of actions) {
+  for (const action of safeActions) {
     switch (action.type) {
       case 'clear':
-        // clear => cria um estado de objeto vazio
         stateCopy = {};
         break;
       case 'addProperties':
-        // validando a existencia de actions e verificando se é um objeto
-        if (action.extraData && typeof action.extraData === 'object') {
-          // Mesclando o estado atual com as novas propriedades
+        if (
+          action.extraData &&
+          typeof action.extraData === 'object' &&
+          !Array.isArray(action.extraData)
+        ) {
           stateCopy = { ...stateCopy, ...action.extraData };
         }
         break;
       case 'removeProperties':
-        // Verificando se a chave é do tipo array
         if (Array.isArray(action.keysToRemove)) {
-          // Cria uma cópia do objeto para manipular
           stateCopy = { ...stateCopy };
 
-          // pegandok a chave e percorrendo o array
           action.keysToRemove.forEach((key) => {
-            if (key in stateCopy) {
-              // deletando a chave do objeto manipulado
-              delete stateCopy[key];
-            }
+            delete stateCopy[key];
           });
         }
         break;
-      default:
     }
 
-    /**
-     * Salvando o objeto modificado em um novo array garantindo a imutabilidade
-     do state  original!
-     */
     history.push({ ...stateCopy });
   }
 
@@ -59,15 +44,3 @@ function transformStateWithClones(state, actions) {
 }
 
 module.exports = transformStateWithClones;
-
-/*
-      Informações sobre a atividade
-  state => Objeto
-  actions => array
-  O objetivo é aplicar cada ação ao estado anterios e retornar um array
-  com a modificação.
-  * addProperties- adicionar todos key: valueos pares fornecidos na
-  * extraDatapropriedade ao novo state;
-  * removeProperties- Remover todas as chaves
-  * fornecidas na keysToRemovematriz do state. (ignorar chaves inexistentes)
-*/
